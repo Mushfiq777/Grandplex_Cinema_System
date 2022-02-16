e75c848b35bfd4e8e166b5fccfc132611b17ae31
syxt00 <sammyyxt@gmail.com>
2022-02-16 17:08:29 +0800

    i printed hi

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main() {
     cout << "Hi Samantha and bhara" << endl;
+    cout << "HI" << endl;
     // Guys just try to print out saying Hi I am Samantha or Bhara if you are bhara :)
     // So that we can get a feel of how version control works
     // Thank you
