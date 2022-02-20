d7445473ffaa25804f3375bff11492d750aac037
syxt00 <sammyyxt@gmail.com>
2022-02-20 22:45:32 +0800

    added the functions to make movie record and displaying it on screen

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include_next "Samantha .h"
 using namespace std;
+
 int main() {
-    cout << "Hi Samantha and bhara" << endl;
-    cout << "HI" << endl;
-    cout << "Hi I Am Bhara" << endl;
-    cout << "Iam so confused" << endl;
-    // Guys just try to print out saying Hi I am Samantha or Bhara if you are bhara :)
-    // So that we can get a feel of how version control works
-    // Thank you
+    //Hardcoded movie list
+    movieRecord("0001", "3D", "John Wick", "Action", "Hall 1", 20.00, 20);
+    movieRecord("0002", "2D", "Ocean's 8", "Comedy", "Hall 5", 15.00, 25);
+    movieRecord("0003","3D",  "Malignant", "Horror", "Hall 2", 20.00, 10);
+    movieRecord("0004", "2D", "The Purge", "Horror", "Hall 3", 15.00, 18);
+
+    displayMovieRecord();
     return 0;
 }
