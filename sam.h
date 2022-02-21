2c79f76ddcda9a49b908ceccbc7dd9f98cbfdb1f
syxt00 <sammyyxt@gmail.com>
2022-02-21 23:24:13 +0800

    added functions to create movie info and display movie list added hardcoded movie info

--- sam.h
+++ sam.h
@@ -12,10 +12,13 @@ struct movieInfo { //creates a template for a node
     int mPrice, mQuantity;
     movieInfo* next;
 };
+
 struct movieInfo* head = NULL;
+
 void movieRecord(string mID, string mType, string mName, string mGenre, string mHall, int mPrice, int mQuantity) {
     //adds a new movie record
     struct movieInfo* newMovie = new struct movieInfo;
+
     newMovie->mID = mID;
     newMovie->mType = mType;
     newMovie->mName = mName;
@@ -23,28 +26,45 @@ void movieRecord(string mID, string mType, string mName, string mGenre, string m
     newMovie->mHall = mHall;
     newMovie->mPrice = mPrice;
     newMovie->mQuantity = mQuantity;
+
     //points the node that holds info of new movie to the same MA of current head; either NULL or first node connected to head
     newMovie->next = head;
+
     //points the head to the MA of the new node created; inserts the node holding new movie info at the beginning of the list
     head = newMovie;
 }
+
 void displayMovieRecord() {
     struct movieInfo* displayMovie;
+
     displayMovie = head;
+
     if (displayMovie == NULL) {
         cout << "  +-----------------------------------------------------------------------------------------------------------+\n";
         cout << "  |                                              NO MOVIES FOUND                                              |\n";
         cout << "  +-----------------------------------------------------------------------------------------------------------+\n";
     }
 
-//header to display movie details
-    cout << " +-----------------------------------------------------------------------------------------------------------+\n"; cout << " | NOW SHOWING |\n"; cout << " +-----------------------------------------------------------------------------------------------------------+\n"; cout << " | Movie ID | | Type | | Movie Name | | Genre | | Hall No. | | Price (RM) | | Quantity |\n"; cout << " +-----------------------------------------------------------------------------------------------------------+";
-    while (displayMovie != NULL) {
-        cout << "\n | " << displayMovie->mID << " | | " << displayMovie->mType << " | | " << displayMovie->mName << " | | " << displayMovie->mGenre << " | | " << displayMovie->mHall << " | | " << displayMovie->mPrice << " | | " << displayMovie->mQuantity << " |" << endl;
-//continue looking for the next node that holds movie info
-        displayMovie = displayMovie->next;
+    //header to display movie details
+    cout << "  +-----------------------------------------------------------------------------------------------------------+\n";
+    cout << "  |                                               NOW SHOWING                                                 |\n";
+    cout << "  +-----------------------------------------------------------------------------------------------------------+\n";
+    cout << "  | Movie ID |    | Type |    |  Movie Name  |    |  Genre  |    | Hall No. |    | Price (RM) |    | Quantity |\n";
+    cout << "  +-----------------------------------------------------------------------------------------------------------+\n";
 
-        cout << " +-----------------------------------------------------------------------------------------------------------+\n"; }
-};
+    while (displayMovie != NULL) {
+        cout << "\n  |   " << displayMovie->mID <<
+             "   |    |  " << displayMovie->mType <<
+             "  |    |  " << displayMovie->mName <<
+             "   |    |  " << displayMovie->mGenre <<
+             " |    |  " << displayMovie->mHall <<
+             "  |    |     "     << displayMovie->mPrice <<
+             "     |    |    " << displayMovie->mQuantity << "    |\n" << endl;
+
+        //continue looking for the next node that holds movie info
+        displayMovie = displayMovie->next;
+    }
+    cout << "  +-----------------------------------------------------------------------------------------------------------+\n";
+}
 
 #endif //DSTR_ASSIGNMENT_SAM_H
