dc96379faed50933341797e23bac21676b5cac49
Bxd7601 <bhara7601@gmail.com>
2022-02-23 00:07:43 +0800

    Bhara's commit once again 2

--- cmake-build-debug/CMakeFiles/DSTR_assignment.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/DSTR_assignment.dir/DependInfo.cmake
@@ -4,20 +4,11 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/mushf/CLionProjects/DSTR_assignment/main.cpp" "C:/Users/mushf/CLionProjects/DSTR_assignment/cmake-build-debug/CMakeFiles/DSTR_assignment.dir/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/bhara1/CLionProjects/DSTR_assignment/main.cpp" "CMakeFiles/DSTR_assignment.dir/main.cpp.obj" "gcc" "CMakeFiles/DSTR_assignment.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
